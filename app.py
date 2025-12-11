15284dbbfeda18cceaf1e00df8d16efb37457004
lali18061988 <lali18061988@gmail.com>
2025-12-11 12:43:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ if st.session_state['user_data']:
             with st.spinner("KI analysiert Preise, Foto und Sprache..."):
                 try:
                     # Input-Liste für die KI vorbereiten
-                    model = genai.GenerativeModel('gemini-1.5-flash')
+                    model = genai.GenerativeModel('gemini-1.5-flash-latest')
                     prompt_parts = []
                     
                     # 1. System-Anweisung & Preisliste
